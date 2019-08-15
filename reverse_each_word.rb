d6454665c3ddc49c33a90f78b0fb5ba997957451
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-15 21:35:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split(' ')
   new_sentence = []
-  string_array.each do |word|
+  string_array.collect do |word|
     new_sentence.push(word.reverse)
   end
   new_sentence.join(' ')
